2ab4206422736a397a7b7ea1a150349fda389cf7
Alejandro Gómez <alejandro@dialelo.com>
2014-01-24 18:34:31 +0100

    Eval clojure expressions in the repl buffer

--- clojure.el
+++ clojure.el
@@ -10,4 +10,19 @@
 (require 'ob-clojure)
 (setq org-babel-clojure-backend 'cider)
 
+
+(defun cider-eval-expression-at-point-in-repl ()
+  (interactive)
+  (let ((form (cider-sexp-at-point)))
+    ;; Strip excess whitespace
+    (while (string-match "\\`\s+\\|\n+\\'" form)
+      (setq form (replace-match "" t t form)))
+    (set-buffer (cider-find-or-create-repl-buffer))
+    (goto-char (point-max))
+    (insert form)
+    (cider-repl-return)))
+
+(add-hook 'clojure-mode-hook
+	  (lambda () (local-set-key (kbd "C-`") 'cider-eval-expression-at-point-in-repl)))
+
 (provide 'clojure)
