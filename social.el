b722a6185bce541123a05f917a32b3710043acde
Alejandro Gómez <alejandro@dialelo.com>
2014-04-02 15:14:37 +0000

    Autojoin to work channel on IRC

--- social.el
+++ social.el
@@ -2,6 +2,6 @@
 (require 'erc)
 (add-to-list 'erc-modules 'notifications)
 
-(setq erc-autojoin-channels-alist '(("freenode.net" "#emacs")))
+(setq erc-autojoin-channels-alist '(("freenode.net" "#kaleidos-gr14")))
 
 (provide 'social)
